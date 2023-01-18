0dc4a5f9ddf566993abcd7eb5995d24fe32b2432
xNoobplays123 <odinstromtronstad@outlook.com>
2023-01-18 11:56:31 +0100

    infoClicked event

--- price/code.js
+++ price/code.js
@@ -1,3 +1,31 @@
+let active = false
+let checked = false
+let icon = document.getElementById("info-icon")
+
 function infoClicked() {
-    console.log("Привет товарищ")
+
+    if (active == true) {
+        icon.src="../src/square-regular.svg";
+        active = false
+        checked = false
+    
+    } else if (active == false) {
+        icon.src="../src/square-check.svg";
+        active = true
+        checked = true
+    }
 }
+
+
+function update() {
+
+    if (active == true) {
+        icon.src="../src/square-regular.svg";
+    } else if (active == false) {
+        icon.src="../src/square-check.svg";
+    }
+
+    requestAnimationFrame(update)
+}
+
+requestAnimationFrame(update)
