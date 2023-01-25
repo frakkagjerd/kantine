b54f682ff0f1b6c1a83b2be29dbcafe06e64a42a
nikeedev <myftr12394@gmail.com>
2023-01-25 12:42:32 +0100

    Added Product class

--- price/code.js
+++ price/code.js
@@ -1,17 +1,58 @@
+
+
+// @nikeedev @xNoobplays123
+
+let product_list = document.querySelector(".image-left")
+
+class Product {
+    name
+    info
+    price
+    constructor(name, info, price, path) {
+        this.name = name;
+        this.info = info;
+        this.price = price;
+
+        product_list.innerHTML += '<img src="' + path + '" alt=' + name + '><p>' + name + ' - ' + price + ' kr</p>'
+
+
+    }
+
+    showInfo() {
+        // Do something to show information about the product...
+        console.log(this.info);
+    }
+}
+
+/*
+
+<img src="../src/box.png" alt="">
+<p>This is a black box - 45 kr</p>
+
+*/
+
+
+const products = [
+new Product("Rislunsj Jordbær", "128 kcal, Allergier: Inneholder melk", "25", "https://bilder.ngdata.no/7038010035463/meny/large.jpg")]
+
+
+
+
 let active = false;
 let information;
 
-let icon = document.getElementById("info-icon")
+let icon = document.getElementById("info-icon");
+
 
 function infoClicked() {
 
     if (active == true) {
-        icon.src="../src/square-check.svg";
+        icon.src = "../src/square-check.svg";
         active = false
         information = false
-    
+
     } else if (active == false) {
-        icon.src="../src/square-regular.svg";
+        icon.src = "../src/square-regular.svg";
         active = true
         information = true
     }
@@ -21,9 +62,9 @@ function infoClicked() {
 function update() {
 
     if (active == true) {
-        icon.src="../src/square-check.svg";
+        icon.src = "../src/square-check.svg";
     } else if (active == false) {
-        icon.src="../src/square-regular.svg";
+        icon.src = "../src/square-regular.svg";
 
     }
     requestAnimationFrame(update)
