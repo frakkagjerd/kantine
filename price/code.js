cd1965c663028e7297aa0c13fcc09061189bb326
nikeedev <myftr12394@gmail.com>
2023-01-25 17:24:59 +0100

    updated everything

--- price/code.js
+++ price/code.js
@@ -8,8 +8,9 @@ class Product {
         this.name = name;
         this.info = info;
         this.price = price;
+        let file_name = path.replace(".png", "")
 
-        product_list.innerHTML += '<br><img style="' + style + '" src="' + path + '" alt=' + name + '><p>' + name + ' - ' + price + ' kr</p>'
+        product_list.innerHTML += `<br><div id="${file_name}"><img style='${style}' src='../products/${path}' alt='${name + " - " + info}'><p>${name} - ${price} kr</p></div>`
 
     }
 
@@ -28,9 +29,12 @@ class Product {
 
 
 const products = []
-products.push(new Product("Rislunsj Jordbær", "128 kcal, Allergier: Inneholder melk", "25", "https://bilder.ngdata.no/7038010035463/meny/large.jpg", "height:200px"))
-products.push(new Product("Rislunsj Pasjon", "128 kcal, Allergier: Inneholder melk", "25", "https://bilder.ngdata.no/7038010069000/meny/large.jpg", "height:200px"))
-products.push(new Product("Rislunsj Pasjon", "128 kcal, Allergier: Inneholder melk", "25", "https://bilder.ngdata.no/7038010069000/meny/large.jpg", "height:200px"))
+products.push(new Product("Rislunsj Jordbær", "128 kcal, Allergier: Inneholder melk", "25", "ris_lunsj_jordbaer.png", "height:200px"))
+products.push(new Product("IsKaffe - Mocha", "128 kcal, Allergier: Inneholder melk", "25", "iskaffe_mocha.png", "height:200px"))
+products.push(new Product("Litago Jordbær", "128 kcal, Allergier: Inneholder melk", "25", "litago_jordbaer.png", "height:200px"))
+products.push(new Product("Litago Sjokolade", "128 kcal, Allergier: Inneholder melk", "25", "litago_sjokolade.png", "height:200px"))
+products.push(new Product("Nudler med kjøttsmak", "128 kcal, Allergier: Inneholder hvete", "25", "nudler_kjoett.png", "height:200px"))
+products.push(new Product("Tine Kakaomelk", "128 kcal, Allergier: Inneholder melk", "25", "tine_kakao.png", "height:200px"))
 
 
 
@@ -55,15 +59,3 @@ function infoClicked() {
 }
 
 
-function update() {
-
-    if (active == true) {
-        icon.src = "../src/square-check.svg";
-    } else if (active == false) {
-        icon.src = "../src/square-regular.svg";
-
-    }
-    requestAnimationFrame(update)
-}
-
-requestAnimationFrame(update)
