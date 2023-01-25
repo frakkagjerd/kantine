bfc0a5a3796516efc70006bc72cfba2e675358a1
StioStudio <simon.s.johanssen@gmail.com>
2023-01-25 12:18:24 +0100

    sidebar

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,13 @@
-let rem = document.querySelector(".sidebar")
-
+var sidebar = false
+function menu(){
+    let rem = document.querySelector(".sidebar")
+    if (sidebar) {
+        rem.style.left = "100%"
+        sidebar = false
+    }
+    else{
+        rem.style.left = "0%"
+        sidebar = true
+    }
+}
 
