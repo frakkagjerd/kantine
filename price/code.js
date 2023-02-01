006b046e9b668dafa7499c21bc2cae76075d31e3
Aleks <90764383+Alekal070@users.noreply.github.com>
2023-02-01 11:00:50 +0100

    Updated product kcal's

--- price/code.js
+++ price/code.js
@@ -29,12 +29,12 @@ class Product {
 
 
 const products = []
-products.push(new Product("Rislunsj Jordbær", "128 kcal, Allergier: Inneholder melk", "13", "ris_lunsj_jordbaer.png", "height:200px"))
-products.push(new Product("IsKaffe - Mocha", "128 kcal, Allergier: Inneholder melk", "25", "iskaffe_mocha.png", "height:200px"))
-products.push(new Product("Litago Jordbær", "128 kcal, Allergier: Inneholder melk", "20", "litago_jordbaer.png", "height:200px"))
-products.push(new Product("Litago Sjokolade", "128 kcal, Allergier: Inneholder melk", "20", "litago_sjokolade.png", "height:200px"))
-products.push(new Product("Nudler med kjøttsmak", "128 kcal, Allergier: Inneholder hvete", "25", "nudler_kjoett.png", "height:200px"))
-products.push(new Product("Tine Kakaomelk", "128 kcal, Allergier: Inneholder melk", "25", "tine_kakao.png", "height:200px"))
+products.push(new Product("Rislunsj Jordbær", "140 kcal, Allergier: Inneholder melk", "13", "ris_lunsj_jordbaer.png", "height:200px"))
+products.push(new Product("IsKaffe - Mocha", "40 kcal, Allergier: Inneholder melk", "25", "iskaffe_mocha.png", "height:200px"))
+products.push(new Product("Litago Jordbær", "55 kcal, Allergier: Inneholder melk", "20", "litago_jordbaer.png", "height:200px"))
+products.push(new Product("Litago Sjokolade", "59 kcal, Allergier: Inneholder melk", "20", "litago_sjokolade.png", "height:200px"))
+products.push(new Product("Nudler med kjøttsmak", "95 kcal, Allergier: Inneholder hvete", "25", "nudler_kjoett.png", "height:200px"))
+products.push(new Product("Tine Kakaomelk", "45 kcal, Allergier: Inneholder melk", "25", "tine_kakao.png", "height:200px"))
 
 
 
