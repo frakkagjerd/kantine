81f0028a79076fd0a9f6fbf14b223035b7cc3248
StioStudio <simon.s.johanssen@gmail.com>
2023-02-08 12:29:40 +0100

    stylish :)

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,15 @@
 var sidebar = false
 function menu(){
+    // let rem = document.createElement("div") 
     let rem = document.querySelector(".sidebar")
     if (sidebar) {
         rem.style.left = "100%"
+        rem.style.borderRadius = "100%"
         sidebar = false
     }
     else{
         rem.style.left = "0%"
+        rem.style.borderRadius = "0%"
         sidebar = true
     }
 }
