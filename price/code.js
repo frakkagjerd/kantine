9c7e375403bdd7d0cb46f65c53bebae6aac1074c
nikeedev <myftr12394@gmail.com>
2023-03-08 11:28:57 +0100

    updates

--- price/code.js
+++ price/code.js
@@ -1,3 +1,6 @@
+console.log("Made with " + "%c\u2764", "color:#fc4444; font-family: ; font-size: 25px;", "by Nikita, Simon, Aleksander and Odin!");
+
+
 let product_list = document.querySelector(".products")
 
 class Product {
--- src/code.js
+++ src/code.js
@@ -1,3 +1,5 @@
+console.log("Made with " + "%c\u2764", "color:#fc4444; font-family: ; font-size: 25px;", "by Nikita, Simon, Aleksander and Odin!");
+
 function setLocation(_location){
     window.location.href=`${_location}`
 }
