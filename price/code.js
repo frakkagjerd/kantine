56a4bf3849410ed5a9a39cde66cdea87b7b637bd
xNoobplays123 <odinstromtronstad@outlook.com>
2023-04-26 13:00:18 +0200

    Fixed icon and formated the products
    Fixed the button to actually work and formated the products to look good.

--- price/code.js
+++ price/code.js
@@ -11,16 +11,15 @@ class Product {
   info;
   price;
   elem = document.createElement("div");
-  infos = document.createElement("div")
-  
+  infos = document.createElement("div");
+
   constructor(name, info, price, path, style) {
     this.name = name;
     this.price = price;
     this.path = path;
     let file_name = path + ".webp";
     let temp = document.createElement("template");
-    temp.innerHTML =
-      `<br>
+    temp.innerHTML = `<br>
       <div style="display: flex; align-items: center; justify-content: center; font-family: 'Roboto';" id="${file_name}">
         <img style='${style}' src='../products/${file_name}' alt='${name} - ${info}'>
         <p style="display:block; float:right">${name} - ${price} kr</p>
@@ -32,15 +31,12 @@ class Product {
       product_list.innerHTML += temp.content.children[i].outerHTML;
     }
     this.infos.innerText = info;
-    
   }
 
   show(bool) {
     if (bool) {
       this.elem.insertBefore(this.infos, this.elem);
-    }
-    else if (!bool) {
-    
+    } else if (!bool) {
     }
   }
 }
@@ -69,25 +65,6 @@ class SpecialProduct {
   }
 }
 
-/* ---- Click Events ----*/
-
-var infoActive;
-var infoIcon = document.querySelector(".info");
-showInfo();
-info()
-
-function info() {
-  if (infoActive == false || infoActive == null) {
-    infoActive = true;
-    infoIcon.src = "./../src/icons/info-icon-white.svg";
-    showInfo();
-  } else if (infoActive == true) {
-    infoActive = false;
-    showInfo();
-    infoIcon.src = "./../src/icons/info-icon-black.svg";
-  }
-}
-
 /* ---- Weekly Product ----*/
 
 let width = 275;
@@ -95,25 +72,9 @@ let width = 275;
 let style = `width: ${width}px;`;
 const weekly = [];
 
-weekly.push(
-  new SpecialProduct(
-    "Brownies",
-    "2077 kcal, Allergier: Gluten",
-    "20",
-    "brownies",
-    `${style}`
-  )
-);
+weekly.push(new SpecialProduct("Brownies", "2077 kcal, Allergier: Gluten", "20", "brownies", `${style}`));
 
-weekly.push(
-  new SpecialProduct(
-    "Brownies2",
-    "657890 kcal, Allergier: Gluten",
-    "20",
-    "brownies",
-    `${style}`
-  )
-);
+weekly.push(new SpecialProduct("Brownies2", "657890 kcal, Allergier: Gluten", "20", "brownies", `${style}`));
 
 /* ---- Products ---- */
 
@@ -123,194 +84,56 @@ var width_wanted = 0;
 
 const products = [];
 
+products.push(new Product("Appelsinjuice", "44 kcal, Allergier: Ingen", "20", "appelsin-juice", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "Appelsinjuice",
-    "44 kcal, Allergier: Ingen",
-    "20",
-    "appelsin-juice",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("Eplejuice", "45 kcal, Allergier: Ingen", "20", "eple-juice", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "Eplejuice",
-    "45 kcal, Allergier: Ingen",
-    "20",
-    "eple-juice",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("Billys Pizza", "44 kcal, Allergier: Gluten, melk", "23", "billys", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "Billys Pizza",
-    "44 kcal, Allergier: Gluten, melk",
-    "23",
-    "billys",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("Go'Morgen® Skogsbær", "119 kcal, Allergier: hvete, melk", "13", "gomorgen-skogsbær", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "Go'Morgen® Skogsbær",
-    "119 kcal, Allergier: hvete, melk",
-    "13",
-    "gomorgen-skogsbær",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("Go'Morgen® Vanilje", "123 kcal, Allergier: hvete, melk", "13", "gomorgen-vanilje", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "Go'Morgen® Vanilje",
-    "123 kcal, Allergier: hvete, melk",
-    "13",
-    "gomorgen-vanilje",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("IsKaffe - Cappuccino", "56 kcal, Allergier: melk", "18", "iskaffe-cappuccino", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "IsKaffe - Cappuccino",
-    "56 kcal, Allergier: melk",
-    "18",
-    "iskaffe-cappuccino",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("IsKaffe - Mocha", "40 kcal, Allergier: melk", "18", "iskaffe-mocha", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "IsKaffe - Mocha",
-    "40 kcal, Allergier: melk",
-    "18",
-    "iskaffe-mocha",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("IsKaffe - Nocciola", "42 kcal, Allergier: melk", "18", "iskaffe-nocciola", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "IsKaffe - Nocciola",
-    "42 kcal, Allergier: melk",
-    "18",
-    "iskaffe-nocciola",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("TINE® IsTe Fersken", "29 kcal, Allergier: Ingen", "20", "iste-fersken", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "TINE® IsTe Fersken",
-    "29 kcal, Allergier: Ingen",
-    "20",
-    "iste-fersken",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("Lettmelk Kakao", "45 kcal, Allergier: melk", "10", "lettmelk-kakao", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "Lettmelk Kakao",
-    "45 kcal, Allergier: melk",
-    "10",
-    "lettmelk-kakao",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("Litago Jordbær", "55 kcal, Allergier: melk", "20", "litago-jordbær", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "Litago Jordbær",
-    "55 kcal, Allergier: melk",
-    "20",
-    "litago-jordbær",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("Litago Sjokolade", "46 kcal, Allergier: melk", "20", "litago-sjokolade", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "Litago Sjokolade",
-    "46 kcal, Allergier: melk",
-    "20",
-    "litago-sjokolade",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("Nudler med kjøttsmak", "95 kcal, Allergier: Gluten, selleri og soya", "25", "nudler-kjøtt", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "Nudler med kjøttsmak",
-    "95 kcal, Allergier: Gluten, selleri og soya",
-    "25",
-    "nudler-kjøtt",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("Nudler med kyllingsmak", "91 kcal, Allergier: Gluten, selleri og soya", "25", "nudler-kylling", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "Nudler med kyllingsmak",
-    "91 kcal, Allergier: Gluten, selleri og soya",
-    "25",
-    "nudler-kylling",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("Risgrøt", "105 kcal, Allergier: melk", "30", "risgrøt", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "Risgrøt",
-    "105 kcal, Allergier: melk",
-    "30",
-    "risgrøt",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("Rislunsj Bringebær", "140 kcal, Allergier: melk", "13", "rislunsj-bringebær", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "Rislunsj Bringebær",
-    "140 kcal, Allergier: melk",
-    "13",
-    "rislunsj-bringebær",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("Rislunsj Jordbær", "140 kcal, Allergier: melk", "13", "rislunsj-jordbær", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "Rislunsj Jordbær",
-    "140 kcal, Allergier: melk",
-    "13",
-    "rislunsj-jordbær",
-    `${style_wanted}`
-  )
-);
+products.push(new Product("Bestikk", "Laget av plastikk", "1", "bestikk", `${style_wanted}`));
 
-products.push(
-  new Product(
-    "Bestikk",
-    "Laget av plastikk",
-    "1",
-    "bestikk",
-    `${style_wanted}`
-  )
-);
+/* ---- Click Events ----*/
 
+var active = true;
 
-function showInfo() {
-  width_wanted = 140;
+function activate() {
+  if (active) {
+    active = false;
+    document.querySelector(".info").src = "./../src/icons/info-icon-white.svg";
 
-  style_wanted = `width: ${width_wanted}px;`;
+    document.querySelector("#appelsin-juice p").innerText = "Bye! GeeksforGeeks people";
+  } else {
+    active = true;
+    document.querySelector(".info").src = "./../src/icons/info-icon-black.svg";
 
-  if (infoActive == true) {
-  } else if (infoActive == false) {
+    document.querySelector("#appelsin-juice p").innerText = "Hii! GeeksforGeeks people";
   }
 }
