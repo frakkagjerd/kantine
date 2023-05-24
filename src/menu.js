83510eae55eb468d87b6aeb75c3d62aafe6770ac
xNoobplays123 <odinstromtronstad@outlook.com>
2023-05-24 12:15:55 +0200

    Blur and overlap
    Made a blured out background for the iframe, and made the iframe overlap the icons.

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,5 @@
-var sidebar = true
+var sidebar = false
+let header = document.querySelector(".header-price")
 
 function menu(){
     // let sidebar_element = document.createElement("div")
@@ -7,10 +8,16 @@ function menu(){
         sidebar_element.style.left = "100%"
         sidebar_element.style.borderRadius = "100%"
         sidebar = false
+
+        header.style.transition = "0.2s"
+        header.style.filter = "blur(0px)"
     }
     else{
         sidebar_element.style.left = "0%"
         sidebar_element.style.borderRadius = "0%"
         sidebar = true
+
+        header.style.transition = "1.5s"
+        header.style.filter = "blur(8px)"
     }
 }
